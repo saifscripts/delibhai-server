8f37d69c830317e589152653b74292abbf5cf9af
Md Saif Ullah <mdsaifullah1302@gmail.com>
2023-11-29 12:23:25 +0600

    refactor isMobilePhone util

--- src/utils/isMobilePhone.js
+++ src/utils/isMobilePhone.js
@@ -1,7 +1,6 @@
 const validator = require('validator');
 
-exports.isMobilePhone = (...locale) => {
-    return (str) => {
-        return validator.isMobilePhone(str, locale)
-    } 
-}
+exports.isMobilePhone =
+    (...locale) =>
+    (str) =>
+        validator.isMobilePhone(str, locale);
