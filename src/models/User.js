d93ad4b9ddffa9f85cee279a7ff1173d25a793bd
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-03-26 07:10:49 +0600

    add default timestamp in at live location

--- src/models/User.js
+++ src/models/User.js
@@ -183,7 +183,7 @@ const userSchema = new mongoose.Schema(
         liveLocation: {
             latitude: Number,
             longitude: Number,
-            timestamp: Number,
+            timestamp: { type: Number, default: () => Date.now() },
         },
         manualLocation: {
             division: String,
