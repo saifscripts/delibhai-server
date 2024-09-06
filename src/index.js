353957e4ef8e2fffebc8383492a6200664ed7c8c
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-09-06 15:26:48 +0600

    deploy

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: [
-      'http://localhost:5173',
+      //   'http://localhost:5173',
       'https://delibhai.com',
       'https://delibhai.vercel.app',
     ],
