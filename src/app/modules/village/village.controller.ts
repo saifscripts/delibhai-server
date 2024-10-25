16a751fee50772c158ef805845c94cec03c4cdd5
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-10-25 11:04:16 +0600

    /village/ -> create village

--- src/app/modules/village/village.controller.ts
+++ src/app/modules/village/village.controller.ts
@@ -3,6 +3,16 @@ import catchAsync from '../../utils/catchAsync';
 import sendResponse from '../../utils/sendResponse';
 import { VillageServices } from './village.service';
 
+const createVillages = catchAsync(async (req, res) => {
+    const result = await VillageServices.createVillages(req.body.villages);
+
+    sendResponse(res, {
+        statusCode: httpStatus.CREATED,
+        message: 'Successfully created villages!',
+        data: result,
+    });
+});
+
 const getVillages = catchAsync(async (req, res) => {
     const result = await VillageServices.getVillages(
         req.params.unionId,
@@ -17,5 +27,6 @@ const getVillages = catchAsync(async (req, res) => {
 });
 
 export const VillageControllers = {
+    createVillages,
     getVillages,
 };
