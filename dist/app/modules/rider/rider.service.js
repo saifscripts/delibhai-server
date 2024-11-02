6408d93c82d5f87e5e482adf5e55431d96ca7098
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-11-03 01:40:55 +0600

    build

--- dist/app/modules/rider/rider.service.js
+++ dist/app/modules/rider/rider.service.js
@@ -239,9 +239,9 @@ const getRiders = (query) => __awaiter(void 0, void 0, void 0, function* () {
                 },
             },
         },
-        // Sort by distance, then by isLive (true first), then by isOnline (true first), then by createdAt
+        // Sort by isOnline (true first), then by distance, then by isLive (true first), then by createdAt
         {
-            $sort: { distance: 1, isLive: -1, isOnline: -1, createdAt: 1 },
+            $sort: { isOnline: -1, distance: 1, isLive: -1, createdAt: 1 },
         },
         // Paginate results
         { $skip: skip },
