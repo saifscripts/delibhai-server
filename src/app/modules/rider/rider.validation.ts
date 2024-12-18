9f7c95a885deddcc428564c164626c82bc0744f0
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-12-19 01:07:50 +0600

    [rider]: update personal info field validations

--- src/app/modules/rider/rider.validation.ts
+++ src/app/modules/rider/rider.validation.ts
@@ -55,16 +55,19 @@ const getRidersValidationSchema = z.object({
 const updateRiderValidationSchema = z.object({
     body: z.object({
         name: z
-            .string()
+            .string({ required_error: 'Name is required!' })
             .trim()
+            .min(1, 'Name is required!')
             .min(3, 'Name must be at least 3 characters long!')
             .optional(),
         fatherName: z
             .string()
             .trim()
-            .refine((value) => value === '' || value.length > 3, {
+            .transform((val) => (val === '' ? null : val))
+            .refine((value) => value === null || value.length > 3, {
                 message: "Father's name must be at least 3 characters long!",
             })
+            .nullable()
             .optional(),
         gender: z
             .enum(['পুরুষ', 'মহিলা', 'অন্যান্য'], {
@@ -76,12 +79,25 @@ const updateRiderValidationSchema = z.object({
                 invalid_type_error: 'Invalid blood group!',
             })
             .optional(),
-        dateOfBirth: z.string().date('Invalid date of birth!').optional(),
+        dateOfBirth: z
+            .string()
+            .transform((val) => (val === '' ? null : val))
+            .refine(
+                (val) =>
+                    val === null || z.string().date().safeParse(val).success,
+                {
+                    message: 'Invalid date of birth!',
+                },
+            )
+            .nullable()
+            .optional(),
         nid: z
             .string()
-            .refine((value) => isNID(value), {
+            .transform((val) => (val === '' ? null : val))
+            .refine((val) => val === null || isNID(val), {
                 message: 'Invalid NID number!',
             })
+            .nullable()
             .optional(),
         nidURL: z.string().url('Invalid NID URL').nullish().optional(),
         contactNo1: z
@@ -90,7 +106,8 @@ const updateRiderValidationSchema = z.object({
             .min(1, 'Mobile number is required!')
             .refine(isMobilePhone, {
                 message: 'Invalid mobile number!',
-            }),
+            })
+            .optional(),
         contactNo2: z
             .string()
             .trim()
